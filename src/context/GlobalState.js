b9ea009a896740ef354ea5576ead4e1e977a79c2
Nazar.M <inazar.m@gmail.com>
2020-07-06 22:58:45 +0500

    Created Context and Reducer
    Created Global Context and its provider
    Wrapped all the components with provider so components have access to state

---
+++ src/context/GlobalState.js
@@ -0,0 +1,28 @@
+import React,{createContext,useReducer} from 'react';
+
+//import the Reducer
+import AppReducer from './AppReducer';
+
+
+//create the initial state
+const initialState={
+    transactions:[
+        {id:1,description:"income",transactionAmount: 500},
+        {id:2,description:"expense",transactionAmount: -100},
+        {id:3,description:"income2",transactionAmount: 500}
+    ]
+}
+
+//create the global context
+export const GlobalContext=createContext(initialState);
+
+//create a provider for the global context
+export const GlobalProvider = ({ children })=>{
+
+    const [state, dispatch] = useReducer(AppReducer,initialState)
+    return(
+        <GlobalContext.Provider value={{transactions: state.transactions}}>
+            {children}
+        </GlobalContext.Provider>
+    )
+}
