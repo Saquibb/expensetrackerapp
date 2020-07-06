f8f1c52a4d8aa853ac2c1872bae41d25279d795c
Nazar.M <inazar.m@gmail.com>
2020-07-06 23:49:48 +0500

    Update GlobalState.js

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -19,7 +19,7 @@ export const GlobalContext=createContext(initialState);
 //create a provider for the global context
 export const GlobalProvider = ({ children })=>{
 
-    const [state, dispatch] = useReducer(AppReducer,initialState)
+    const [state] = useReducer(AppReducer,initialState)
     return(
         <GlobalContext.Provider value={{transactions: state.transactions}}>
             {children}
